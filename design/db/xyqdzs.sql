969ad9c23179781e5218a982cb71da153dc10d8e
chenzhixing <chenzhixing@cemobile.cn>
2016-02-18 15:02:02 +0800

    修改库表设计（将大部分的Int类型转成bigint类型）

--- design/db/xyqdzs.sql
+++ design/db/xyqdzs.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2016/2/16 13:49:18                           */
+/* Created on:     2016/2/18 14:40:25                           */
 /*==============================================================*/
 
 
@@ -45,11 +45,11 @@ drop table if exists lab_teacher;
 /*==============================================================*/
 create table LAB_TERM
 (
-   term_id              int not null auto_increment comment '学期ID',
+   term_id              bigint not null auto_increment comment '学期ID',
    term_name            char(30),
    start_date           date comment '开始时间',
    end_date             date comment '结束时间',
-   college_id           int,
+   college_id           bigint,
    create_date          date comment '创建时间',
    edite_date           date comment '修改时间',
    del                  int comment '删除标识',
@@ -63,12 +63,12 @@ alter table LAB_TERM comment '学期表';
 /*==============================================================*/
 create table LAB_TIME
 (
-   id                   int not null auto_increment comment 'id',
+   id                   bigint not null auto_increment comment 'id',
    section              int comment '节数',
-   term_id              int comment '学期',
+   term_id              bigint comment '学期',
    start_date           time comment '开始时间',
    end_date             time comment '结束时间',
-   college_id           int,
+   college_id           bigint,
    create_date          date comment '创建时间',
    edit_date            date comment '修改时间',
    del                  int comment '删除标识',
@@ -82,18 +82,22 @@ alter table LAB_TIME comment '时间表';
 /*==============================================================*/
 create table lab_bluetooth_dev
 (
-   dev_id               int not null auto_increment comment 'ID',
-   dev_number           int comment '蓝牙编号',
+   dev_id               bigint not null auto_increment comment 'ID',
+   dev_number           bigint comment '蓝牙编号',
    manufactor           char(200) comment '厂家',
    status               int comment '状态',
-   position_id          int,
+   position_id          bigint,
    uuid                 char(35),
-   major                int,
-   minor                int,
+   major                bigint,
+   minor                bigint,
    dev_type             char(50),
    create_date          date comment '创建时间',
    edit_date            date comment '修改时间',
    del                  int comment '删除标识',
+   dev_sn               char,
+   dev_mac              char(20),
+   measuredPower        int(11),
+   name                 char(30),
    primary key (dev_id)
 );
 
@@ -104,18 +108,18 @@ alter table lab_bluetooth_dev comment '蓝牙设备';
 /*==============================================================*/
 create table lab_check_work
 (
-   check_id             int not null auto_increment comment '考勤id',
-   class_id             int comment '班级ID',
-   curriculum_id        int comment '课程id',
-   curriculum_name      int,
+   check_id             bigint not null auto_increment comment '考勤id',
+   class_id             bigint comment '班级ID',
+   curriculum_id        bigint comment '课程id',
+   curriculum_name      char(100),
    status               char comment '考勤状态',
-   student_id           int comment '学生id',
+   student_id           bigint comment '学生id',
    weekNo               int,
    weekday              int,
    section              int comment '节数',
    start_date           date comment '上课签到时间',
    end_date             date comment '下课签到时间',
-   term_id              int comment '学期',
+   term_id              bigint comment '学期',
    create_date          date comment '创建时间',
    edit_date            date comment '修改时间',
    del                  int comment '删除标识',
@@ -129,13 +133,13 @@ alter table lab_check_work comment '考勤记录';
 /*==============================================================*/
 create table lab_class
 (
-   class_id             int not null auto_increment comment '班级id',
+   class_id             bigint not null auto_increment comment '班级id',
    calss_name           char(50) not null comment '班级名称',
-   college_id           int not null,
-   department_id        int not null comment '院系ID',
-   major_id             int not null comment '专业',
-   headmaster           int comment '班主任',
-   instructor           int comment '辅导员',
+   college_id           bigint not null,
+   department_id        bigint not null comment '院系ID',
+   subject_id           bigint not null comment '专业',
+   headmaster           bigint comment '班主任',
+   instructor           bigint comment '辅导员',
    grade                int not null comment '级',
    create_date          date comment '创建时间',
    edit_date            date comment '修改时间',
@@ -150,7 +154,7 @@ alter table lab_class comment '班级';
 /*==============================================================*/
 create table lab_college
 (
-   college_id           int not null auto_increment,
+   college_id           bigint not null auto_increment,
    college_name         char(30) not null,
    short_name           char(10),
    college_profile      varchar(500),
@@ -165,13 +169,13 @@ create table lab_college
 /*==============================================================*/
 create table lab_course_list
 (
-   course_list_id       int not null auto_increment comment '课程表ID',
-   curriculum_id        int,
+   course_list_id       bigint not null auto_increment comment '课程表ID',
+   curriculum_id        bigint,
    weekday              int not null comment '周几',
    section              int not null comment '节数（第几节）',
-   class_id             int not null,
-   term_id              int not null comment '学期',
-   position_id          int,
+   class_id             bigint not null,
+   term_id              bigint not null comment '学期',
+   position_id          bigint,
    course_type          int not null,
    create_date          date comment '创建时间',
    edit_date            date comment '修改时间',
@@ -186,16 +190,14 @@ alter table lab_course_list comment '课程表';
 /*==============================================================*/
 create table lab_curriculum_name
 (
-   curriculum_id        int not null auto_increment comment '课程id',
+   curriculum_id        bigint not null auto_increment comment '课程id',
    curriculum_name      char(100) comment '课程名称',
-   teacher_id           int comment '任课老师ID',
+   teacher_id           bigint comment '任课老师ID',
    teacher_name         char(20),
    start_week           int comment '开始时间（周）',
    end_week             int comment '结束时间（周）',
-   subject_id           int not null,
-   college_id           int not null,
-   department_id        int not null comment '院系ID',
-   term_id              int not null comment '学期id',
+   subject_id           bigint not null,
+   term_id              bigint not null comment '学期id',
    level                int comment '级',
    course_type          int,
    create_date          date comment '创建时间',
@@ -211,11 +213,11 @@ alter table lab_curriculum_name comment '课程名称';
 /*==============================================================*/
 create table lab_department
 (
-   department_id        int not null auto_increment,
+   department_id        bigint not null auto_increment,
    department_name      char(30) not null,
    short_name           char(10),
    department_profile   varchar(500),
-   college_id           int not null,
+   college_id           bigint not null,
    create_date          date,
    edit_date            date,
    del                  int,
@@ -227,18 +229,16 @@ create table lab_department
 /*==============================================================*/
 create table lab_exam
 (
-   exam_id              int not null auto_increment,
-   curriculum_id        int,
+   exam_id              bigint not null auto_increment,
+   curriculum_id        bigint,
    curriculum_name      char(100),
    start_time           time,
    end_time             time,
    week                 int,
-   college_id           int not null,
-   department_id        int not null,
-   subject_id           int not null,
+   subject_id           bigint not null,
    level                int,
-   term_id              int,
-   teacher_id           int,
+   term_id              bigint,
+   teacher_id           bigint,
    create_date          date,
    edit_date            date,
    del                  int,
@@ -250,15 +250,15 @@ create table lab_exam
 /*==============================================================*/
 create table lab_modify_course
 (
-   modify_id            int not null auto_increment comment '课程表修改ID',
-   curriculum_id        int,
+   modify_id            bigint not null auto_increment comment '课程表修改ID',
+   curriculum_id        bigint,
    weekday              int not null comment '周几',
    section              int not null comment '节数',
-   term_id              int not null comment '学期',
-   class_id             int not null,
-   position_id          int,
+   term_id              bigint not null comment '学期',
+   class_id             bigint not null,
+   position_id          bigint,
    course_type          int not null,
-   course_id            int comment '课程表ID',
+   course_id            bigint comment '课程表ID',
    create_date          date comment '创建时间',
    edit_date            date comment '修改时间',
    del                  int comment '删除标识',
@@ -272,10 +272,10 @@ alter table lab_modify_course comment '课程修改表';
 /*==============================================================*/
 create table lab_motion_trail
 (
-   trail_d              int not null auto_increment comment 'id',
-   student_id           int comment '学生id',
-   position_id          int comment '位置',
-   residence_tim        int,
+   trail_d              bigint not null auto_increment comment 'id',
+   student_id           bigint comment '学生id',
+   position_id          bigint comment '位置',
+   residence_tim        bigint,
    start_time           date,
    leave_time           date,
    create_date          date comment '创建时间',
@@ -289,15 +289,15 @@ alter table lab_motion_trail comment '轨迹表';
 /*==============================================================*/
 create table lab_notice
 (
-   notice_id            int not null auto_increment comment 'ID',
+   notice_id            bigint not null auto_increment comment 'ID',
    content              varchar(1000) comment '内容',
    title                char(100) comment '标题',
    display_date         int comment '展示时间',
-   teacher_id           int comment '创建人id',
+   teacher_id           bigint comment '创建人id',
    source               char,
    class_id             char comment '接收班级',
-   department_id        int,
-   college_id           int,
+   department_id        bigint,
+   college_id           bigint,
    top                  int comment '置顶标识',
    creat_date           date comment '创建时间',
    edit_date            date comment '修改时间',
@@ -312,7 +312,7 @@ alter table lab_notice comment '通知';
 /*==============================================================*/
 create table lab_position
 (
-   position_id          int not null auto_increment comment '位置ID',
+   position_id          bigint not null auto_increment comment '位置ID',
    place                char(100) comment '场地',
    number               char(20) comment '编号',
    position_type        int comment '位置类型',
@@ -329,27 +329,25 @@ alter table lab_position comment '位置';
 /*==============================================================*/
 create table lab_student
 (
-   student_id           int not null auto_increment comment '学生ID',
-   student_number       int not null comment '学号',
+   student_id           bigint not null auto_increment comment '学生ID',
+   student_number       bigint not null comment '学号',
    student_name         char comment '姓名',
    nickname             char(50) comment '昵称',
    sex                  bit comment '性别',
    age                  int(3) comment '年龄',
    nation               char(20) comment '民族',
-   phone                int(11) comment '手机号码',
+   phone                bigint(11) comment '手机号码',
    id_card              char(18) comment '身份证号码',
-   type                 int comment '职位',
+   type                 bigint comment '职位',
    email                char comment 'Email',
-   major_id             int comment '专业',
-   class_id             int,
-   department_id        int comment '学系ID',
-   college_id           int comment '学院id',
+   class_id             bigint,
    head_image           char comment '头像',
    note                 varchar(500) comment '备注',
    password             char comment '密码',
    creat_date           date comment '创建时间',
    edit_date            date comment '修改时间',
    del                  int comment '删除标识',
+   useri_id             bigint,
    primary key (student_id)
 );
 
@@ -360,8 +358,8 @@ alter table lab_student comment '学生信息';
 /*==============================================================*/
 create table lab_student_phone
 (
-   mac_id               int not null auto_increment comment 'id',
-   student_id           int not null comment '学生id',
+   mac_id               bigint not null auto_increment comment 'id',
+   student_id           bigint not null comment '学生id',
    mac                  char comment '手机物理地址',
    creat_date           date comment '创建时间',
    edit_date            date comment '修改时间',
@@ -376,12 +374,12 @@ alter table lab_student_phone comment '手机';
 /*==============================================================*/
 create table lab_subject
 (
-   subject_id           int not null auto_increment,
+   subject_id           bigint not null auto_increment,
    subject_name         char(30) not null,
    short_name           char(10),
    subject_profile      varchar(500),
-   college_id           int not null,
-   department_id        int not null,
+   college_id           bigint not null,
+   department_id        bigint not null,
    create_date          date,
    edit_date            date,
    del                  int,
@@ -393,26 +391,27 @@ create table lab_subject
 /*==============================================================*/
 create table lab_teacher
 (
-   teacher_id           int not null auto_increment comment '教职工ID',
-   teacher_number       int not null comment '工号',
+   teacher_id           bigint not null auto_increment comment '教职工ID',
+   teacher_number       bigint not null comment '工号',
    teacher_name         char(20) comment '姓名',
    nickname             char(50) comment '昵称',
    sex                  bit comment '性别',
-   age                  int(3) comment '年龄',
+   age                  bigint(3) comment '年龄',
    nation               char(20) comment '民族',
-   phone                int(11) comment '手机号码',
-   id_card              int(18) comment '身份证号码',
+   phone                bigint(11) comment '手机号码',
+   id_card              bigint(18) comment '身份证号码',
    email                char comment 'Email',
-   professional         int,
-   college_id           int,
-   department_id        char comment '院系ID',
+   professional         bigint,
+   college_id           bigint,
+   department_id        bigint comment '院系ID',
    head_image           char comment '头像',
-   role                 int comment '角色',
+   role                 bigint comment '角色',
    note                 varchar(500) comment '备注',
    password             char comment '密码',
    creat_date           date comment '创建时间',
    edit_date            date comment '修改时间',
    del                  int comment '删除标识',
+   user_id              bigint,
    primary key (teacher_id)
 );
 
@@ -420,6 +419,9 @@ alter table lab_teacher comment '教职工信息';
 
 alter table lab_bluetooth_dev add constraint FK_Reference_11 foreign key (position_id)
       references lab_position (position_id) on delete restrict on update restrict;
+
+alter table lab_class add constraint FK_Reference_12 foreign key (subject_id)
+      references lab_subject (subject_id) on delete restrict on update restrict;
 
 alter table lab_course_list add constraint FK_Reference_4 foreign key (curriculum_id)
       references lab_curriculum_name (curriculum_id) on delete restrict on update restrict;
